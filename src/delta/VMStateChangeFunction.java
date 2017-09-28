bb42b1c7335474d38a06616e0aa60b58efbc31be
Final Calibur <fmorris2@oswego.edu>
2017-09-28 02:50:29 -0400

    Changed method name

--- src/delta/VMStateChangeFunction.java
+++ src/delta/VMStateChangeFunction.java
@@ -8,11 +8,11 @@ import vending_machine.data.VMInput;
 
 public class VMStateChangeFunction {
 	public static void execute(VendingMachine vendingMachine, VMInput[] inputBag) {
-		changeState(vendingMachine);
-		Arrays.stream(inputBag).forEach(i -> i.INPUT_EFFECT.accept(vendingMachine));
+		checkForChangeAndCoffee(vendingMachine); //change & coffee
+		Arrays.stream(inputBag).forEach(i -> i.INPUT_EFFECT.accept(vendingMachine)); //input
 	}
 	
-	private static void changeState(VendingMachine vendingMachine) {
+	private static void checkForChangeAndCoffee(VendingMachine vendingMachine) {
 		if(vendingMachine.isChangePressed()) {
 			Arrays.stream(ChangeUtils.calculateChange(vendingMachine))
 				.map(output -> Arrays.stream(VMInput.values()).filter(i -> i.name().equals(output.name())).findFirst().get())
